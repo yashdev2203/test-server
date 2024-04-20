a1e7dec8856885ad70226715689ea738f16cabf8
YASH <92169629+YASH2221@users.noreply.github.com>
2024-04-20 21:31:24 +0530

    firstcommit

--- config/mongo.js
+++ config/mongo.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose')
 
 module.exports = ()=>{
     const connect = ()=>{
-        mongoose.connect(process.env.MONGO_URI).then((e)=>{
+        mongoose.connect(process.env.MONGO_URI, {
+            maxPoolSize: 250,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            connectTimeoutMS: 30000,
+            socketTimeoutMS: 30000,
+          },).then((e)=>{
             console.log('Connect on mongo')
         }).catch((e)=>{
             console.log(e)
